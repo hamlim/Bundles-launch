aef605bffc7e33bd23ad726be8d8152cf4b5ae6b
hamlim <matthamlin@lunge-63.dynamic2.rpi.edu>
2015-12-15 17:43:55 -0500

    Very simple login working

--- bapp/js/main.js
+++ bapp/js/main.js
@@ -6,5 +6,37 @@
 
 //handle login and signup forms
 // login -> login.js  Signup -> signup.js
-
+var d = document, w = window;
 //this file is for whole app code
+$(d).ready(function() {
+
+    // ------------------------------------------------------------------------
+    //first we want to verify that the user is logged in
+    if(localStorage.length = 0){
+        //they are not logged in or the localStorage was cleared
+        w.location.href = "./index.html";
+    } else {
+        var user = JSON.parse(localStorage.getItem("currentUser"));
+        // the user is logged in
+        // --------------------------------------------------------------------
+        // render the top navbar properly
+        var navbar = d.getElementById('navbar-mustache');
+        var template = "<i class='fa fa-bars'></i> {{ username }} ";
+        var rendered = Mustache.render(template, user);
+        navbar.innerHTML = rendered;
+
+        // --------------------------------------------------------------------
+        // need a for loop to get each of the bunldes the user has
+        var allUsersBundles = [];
+
+    }
+});
+
+function logoutUser() {
+    //user clicked on the logout button/link
+    //we want to redirect to the index page, and also clear the localStorage
+    //first remove the data
+    localStorage.removeItem("currentUser");
+    user = "";
+    w.location.href = "./index.html";
+}
