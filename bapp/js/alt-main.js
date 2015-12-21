ff4bf8de4e16082b52cdee5070cdb78f6cbb351f
hamlim <matthamlin@Neil-Hamlins-MacBook-Pro.local>
2015-12-21 14:02:06 -0800

    some fixes

--- bapp/js/alt-main.js
+++ bapp/js/alt-main.js
@@ -1,25 +1,31 @@
 var currentLinks = [],
-  d = document
+  d = document,
+  sometest = false,
   w = window;
 $(d).ready( function() {
     //ok when someone clicks on the div, we want a small form to pop-below the div and to allow them to input a links
     //this form will have a simple label, a lightblue? background, and an input field, and another button
     //when the last button in the form above is clicked the link is pushed to a simple status array, stored as status[link-number]=link
-    $('.plus-button').click(function () {
+    $('.can-click').on('click', function () {
         //ok time to append a small form
         var newBtn = d.createElement('div');
-        newBtn.setAttribute('class', 'plus-button two columns');
+        newBtn.setAttribute('class', 'plus-button two columns can-click');
         var plusIcon = d.createElement('i');
         plusIcon.setAttribute('class', 'fa fa-plus plus-icon');
         newBtn.appendChild(plusIcon);
+        var elem = this;
         //ok so `This` refers to the dom node
-        UIkit.modal.prompt("Link:", '', function(value){
+        UIkit.modal.prompt("Link: ", '', function(value){
             //here we have access to value which will be a link
-            currentLinks.push(value);
+            console.log(value + " 19");
+            if(value != ''){
+                sometest = true;
+                currentLinks.push(value);
+                elem.className = "plus-button two columns activated";
+                elem.innerHTML = "<i class='fa fa-check plus-icon'></i>";
+                elem.parentNode.appendChild(newBtn);
+            }
         });
-        this.innerHTML = "<i class='fa fa-check plus-icon'></i>";
-        this.parentNode.appendChild(newBtn);
-
     });
     console.log(currentLinks);
 });
