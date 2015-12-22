da52213370bcef89019ffbfaacbba9d3db0937e9
hamlim <matthamlin@Neil-Hamlins-MacBook-Pro.local>
2015-12-21 20:56:51 -0800

    fixed Hype

--- bapp/js/b-main.js
+++ bapp/js/b-main.js
@@ -2,70 +2,95 @@ var currentLinks = [],
   d = document,
   w = window;
 $(d).ready( function() {
-    //ok so we want people to add content, when they click on the .can-click divs
-    //a modal pops up that allows them to add content to their bundle
-    $('#plus-button-container').on('click','div.can-click' , function () {
-        //ok time to append a small form
-        var newBtn = d.createElement('div');
-        newBtn.setAttribute('class', 'plus-button two columns can-click');
-        var plusIcon = d.createElement('i');
-        plusIcon.setAttribute('class', 'fa fa-plus plus-icon');
-        newBtn.appendChild(plusIcon);
-        var linkli = d.createElement('li');
-        var linkanchor = d.createElement('a');
-        var elem = this;
-        //ok so `This` refers to the dom node
-        UIkit.modal.prompt("Link: ", '', function(value){
-            //here we have access to value which will be a link
-            var alreadycheck = true;
-            for(var i=0; i<currentLinks.length; i++){
-                if (value === currentLinks[i]){
-                    alreadycheck = false;
-                    UIkit.modal.alert("You have already added that Link!");
+    // ------------------------------------------------------------------------
+    //first we want to verify that the user is logged in
+    if(localStorage.length = 0){
+        //they are not logged in or the localStorage was cleared
+        w.location.href = "./index.html";
+    } else {
+        var user = JSON.parse(localStorage.getItem("currentUser"));
+        // the user is logged in
+        // --------------------------------------------------------------------
+        // render the top navbar properly
+        var navbar = d.getElementById('navbar-mustache');
+        var template = "<i class='fa fa-bars'></i> {{ username }} ";
+        var rendered = Mustache.render(template, user);
+        navbar.innerHTML = rendered;
+
+        // --------------------------------------------------------------------
+        // need a for loop to get each of the bunldes the user has GO
+        var allUsersBundles = [];
+
+        // --------------------------------------------------------------------
+        // Handle the make form
+        //number of content
+
+        //ok so we want people to add content, when they click on the .can-click divs
+        //a modal pops up that allows them to add content to their bundle
+        $('#plus-button-container').on('click','div.can-click' , function () {
+            //ok time to append a small form
+            var newBtn = d.createElement('div');
+            newBtn.setAttribute('class', 'plus-button two columns can-click');
+            var plusIcon = d.createElement('i');
+            plusIcon.setAttribute('class', 'fa fa-plus plus-icon');
+            newBtn.appendChild(plusIcon);
+            var linkli = d.createElement('li');
+            var linkanchor = d.createElement('a');
+            var elem = this;
+            //ok so `This` refers to the dom node
+            UIkit.modal.prompt("Link: ", '', function(value){
+                //here we have access to value which will be a link
+                var alreadycheck = true;
+                for(var i=0; i<currentLinks.length; i++){
+                    if (value === currentLinks[i]){
+                        alreadycheck = false;
+                        UIkit.modal.alert("You have already added that Link!");
+                    }
+                }
+                if(value != '' && alreadycheck){
+                    currentLinks.push(value);
+                    elem.className = "plus-button two columns activated";
+                    elem.innerHTML = "<i class='fa fa-check plus-icon'></i>";
+                    elem.parentNode.appendChild(newBtn);
+                    linkanchor.setAttribute('href', value);
+                    linkanchor.setAttribute('class', 'link');
+                    linkanchor.innerHTML = value;
+                    linkli.appendChild(linkanchor);
+                    var dellink = d.createElement('span');
+                    dellink.setAttribute('class',  'delete-link');
+                    dellink.setAttribute('data-bundle-link', value);
+                    dellink.innerHTML = "  Click here to delete this link!";
+                    linkli.appendChild(dellink);
+                    d.getElementById('current-links').appendChild(linkli);
+                }
+            });
+        });
+        $('#current-links').on('click','span.delete-link' , function () {
+            //ok so you clicked on the p tag, now we want to remove the li element (this.parentNode), the link within the array, and a checked off div above
+            var parentElem = this.parentNode;
+            var lenOfCurrentLinks = currentLinks.length;
+            for (var k=0; k<lenOfCurrentLinks; k++){
+                if(currentLinks[k] === this.getAttribute('data-bundle-link')){
+                    //ok now we want to pop that from the array
+                    currentLinks.splice(k,1);
+                    //now we want to remove the li element (parentElem)
+                    var parentOfParentElem = parentElem.parentNode;
+                    parentOfParentElem.removeChild(parentElem);
+                    //now we want to remove one of the activated divs above
+                    d.getElementsByClassName('activated')[0].parentNode.removeChild(d.getElementsByClassName('activated')[0]);
                 }
             }
-            if(value != '' && alreadycheck){
-                currentLinks.push(value);
-                elem.className = "plus-button two columns activated";
-                elem.innerHTML = "<i class='fa fa-check plus-icon'></i>";
-                elem.parentNode.appendChild(newBtn);
-                linkanchor.setAttribute('href', value);
-                linkanchor.setAttribute('class', 'link');
-                linkanchor.innerHTML = value;
-                linkli.appendChild(linkanchor);
-                var dellink = d.createElement('span');
-                dellink.setAttribute('class',  'delete-link');
-                dellink.setAttribute('data-bundle-link', value);
-                dellink.innerHTML = "  Click here to delete this link!";
-                linkli.appendChild(dellink);
-                d.getElementById('current-links').appendChild(linkli);
-            }
+
         });
-    });
-    $('#current-links').on('click','span.delete-link' , function () {
-        //ok so you clicked on the p tag, now we want to remove the li element (this.parentNode), the link within the array, and a checked off div above
-        var parentElem = this.parentNode;
-        var lenOfCurrentLinks = currentLinks.length;
-        for (var k=0; k<lenOfCurrentLinks; k++){
-            if(currentLinks[k] === this.getAttribute('data-bundle-link')){
-                //ok now we want to pop that from the array
-                currentLinks.splice(k,1);
-                //now we want to remove the li element (parentElem)
-                var parentOfParentElem = parentElem.parentNode;
-                parentOfParentElem.removeChild(parentElem);
-                //now we want to remove one of the activated divs above
-                d.getElementsByClassName('activated')[0].parentNode.removeChild(d.getElementsByClassName('activated')[0]);
-            }
-        }
-
-    });
-    console.log(currentLinks);
+        console.log(currentLinks);
 
 
-    //now we want to handle the big blue button
-    //first we want to make sure that there is some content in the currentLinks array
-    //then we want to check that there has been some text input in the Bundle Name input
+        //now we want to handle the big blue button
+        //first we want to make sure that there is some content in the currentLinks array
+        //then we want to check that there has been some text input in the Bundle Name input
+
+
+    }
+
     
-
-
 });
