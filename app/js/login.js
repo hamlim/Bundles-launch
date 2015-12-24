6db9c8f2482d6a9fe126373d3305cc334e492812
hamlim <matthamlin@Neil-Hamlins-MacBook-Pro.local>
2015-12-24 12:01:41 -0800

    fixes?

--- app/js/login.js
+++ app/js/login.js
@@ -23,7 +23,7 @@ $(document).ready(function (){
                         //now we also want to get all the lists that the user owns
                         var xhr = new XMLHttpRequest();
                         // xhr.withCredentials = true;
-                        xhr.open("GET", "https://api.airtable.com/v0/appqxUoD7s3dL1gtc/Lists");
+                        xhr.open("GET", "https://api.airtable.com/v0/appqxUoD7s3dL1gtc/testLists");
                         xhr.setRequestHeader("authorization", "Bearer keyIye3zskPSBMQ6Q");
                         xhr.addEventListener("readystatechange", function () {
                             if (this.readyState === 4) {
